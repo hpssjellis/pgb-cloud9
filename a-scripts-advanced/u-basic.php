001eaa275c774be0a55dfaea891d221a9383176e
Jeremy Ellis <jellis@hpssapps.com>
2014-12-18 11:27:05 -0800

    Update and rename betterBasic.php to u-basic.php

--- a-scripts-advanced/u-basic.php
+++ a-scripts-advanced/u-basic.php
@@ -6,10 +6,25 @@
     $myDatabase = "c9";                          // Your database name you want to connect to
     $myTable = "c9table";                         // Your database table
     $myPort = 3306;                                // The port #. It is always 3306
+    
+    
+     // note this is using the new functions for mysql notice the 'i' mysqli 
+     
+     
     $myConnection = mysqli_connect($myHost, $myUser, $myPass, $myDatabase, $myPort)or die(mysql_error());
+     
+     
+     
+     
+     
     // And now to perform a simple query to make sure it's working
     $myQuery = "SELECT * FROM $myTable";
     $myResult = mysqli_query($myConnection, $myQuery);
+     
+     
+     
+     
+     
     while ($myRow = mysqli_fetch_assoc($myResult)) {
         echo "The ID is: " . $myRow['myID'] . " and the Username is: " . $myRow['myName']. "<br>";
     }
