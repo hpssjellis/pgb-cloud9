2ea300e3d44723333d0857153b67a466eb14331a
Jeremy Ellis <keyfreemusic@gmail.com>
2016-02-01 11:25:21 -0800

    Update backup-zip.sh

--- backup-zip.sh
+++ backup-zip.sh
@@ -1,5 +1,12 @@
+#!/bin/bash  
 
+#  only need to run this script with the command (do not type the #)
+#  bash a03-create-gradle-rocksetta.sh
+# best in cloud 9 to just right click this file and select run
 
 
 
 zip -r myCloud9-Backup.zip ~/workspace
+
+
+echo "Right-click myCloud9-Backup.zip  --> select download to backup your workspace to to windows"
