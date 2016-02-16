05c74bc4153c1e2e5c3d7927be73a3b1ca7b75ee
Jeremy Ellis <keyfreemusic@gmail.com>
2016-02-15 21:58:45 -0800

    Update backup-www-zip.sh

--- backup-www-zip.sh
+++ backup-www-zip.sh
@@ -1,7 +1,7 @@
 #!/bin/bash  
 
 #  only need to run this script with the command (do not type the #)
-#  bash a03-create-gradle-rocksetta.sh
+#  bash backup-www-zip.sh
 # best in cloud 9 to just right click this file and select run
 
 
@@ -9,4 +9,4 @@
 zip -r my-www.zip ~/workspace/www
 
 
-echo "Right-click my-www.zip  --> select download to backup your workspace to to windows"
+echo "Right-click my-www.zip  --> select download to backup your workspace to windows"
